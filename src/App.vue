31d9107281f01c0acbfca7ad9e2107419d96b0ce
gabriel-moura <gabriel-moura11@live.com>
2020-10-22 22:26:39 -0300

    Adiconando props, databinds one way  e two way

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
   <div id="app">
       <h1>Estudos</h1>
-      <Cliente/>
-      <Produto/>
+      <Cliente nome="Gabriel" numero="2123123123" email="gab@.com" idade=20 descricao="dsadada"/>
+      <Cliente  nome="Junior" numero="2321312331" email="dasda@a.com" idade=19 descricao="dasdasd"/>
+      <Cliente  nome="José" numero="3123123312" email="sda@a.com" idade=18 descricao="dasdsada"/>
+      <Cliente  nome="Marcio" numero="312312312" email="sa@as.com" idade=17 descricao="dasdasda"/>
+      <Cliente  nome="Maria" numero="31231231112" email="mdsa@sa.com" idade=16 descricao="gdfsfas"/>
   </div>
 </template>
 
 <script>
 
 import Cliente  from './components/Cliente'
-import Produto from './components/Produto'
 export default {
   name: 'App',
   components:{
-    Cliente,Produto 
+    Cliente 
   }
 }
 </script>
 
 <style>
 #app {
-  
+  background-color:lightblue ;
+  padding:1%;
 }
 </style>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,18 +1,41 @@
 <template>
-<div>
-    <h2>Cliente aqui - component aqui </h2>
-    <h3>aaa</h3>
+<div id="cliente">
+            <h4>Nome:{{nome}}</h4>
+            <hr>
+            <p>{{descricao}}</p>
+            <hr>
+            <p>Numero:{{numero}}</p>
+            <p>Email:{{email}}</p>
+            <p>Idade:{{idade}}</p>
+  
 </div> 
 </template>
 <script>
 export default {
-    
+    data(){
+            return{
+                
+                }
+
+    },
+    props:{
+        nome:String,
+        numero:String,
+        email:String,
+        idade:Number,
+        descricao:String
+
+    }
 }
 </script>
 
 <style scoped>
-div{
-    background : linear-gradient(329.54deg,#76d6df 0%,#1bb1bf 100%)
+div #cliente{
+    background : linear-gradient(329.54deg,#76d6df 0%,#1bb1bf 100%);
+    padding:1%;
+    width: 300px;
+    height: 250px;
+    margin-top:1%;
 }
 </style>
 
